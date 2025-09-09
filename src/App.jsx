2ce90e117d756dc1567fe7f416c7c38ff058cf92
Hari Prasad R <r.hari4686@gmail.com>
2025-09-09 22:49:51 +0530

    Rename cart.jsx to Cart.jsx for case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
-import Cart from './pages/cart/cart.jsx'
+import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Footer from './components/Footer/Footer.jsx'
 
